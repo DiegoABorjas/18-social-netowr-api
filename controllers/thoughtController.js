37c04439497834e27e5e14c9c80cfa3cfaf1e250
Diego Borjas <diegoborjas@gmail.com>
2023-07-29 19:58:18 -0500

    finished adding thought routes as well as reaction routes

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,7 +1,6 @@
 const { Thought, User } = require('../models')
 
 module.exports = {
-
   // get all thoughts
   async getThoughts(req, res) {
     try {
@@ -30,6 +29,16 @@ module.exports = {
   async createThought(req, res) {
     try {
       const dbThoughtData = await Thought.create(req.body)
+      const user = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: dbThoughtData._id } },
+        { new: true }
+      )
+      if (!user) {
+        return res.status(404).json({
+          message: 'Thought created, but found no user with that ID',
+        })
+      }
       res.json(dbThoughtData)
     } catch (err) {
       res.status(500).json(err)
@@ -39,34 +48,88 @@ module.exports = {
   // Delete a thought
   async deleteThought(req, res) {
     try {
-      const thought = await Thought.findOneAndDelete({ _id: req.params.id });
+      const thought = await Thought.findOneAndRemove({ _id: req.params.id });
 
       if (!thought) {
-        res.status(404).json({ message: 'No thought with that ID' });
+        return res.status(404).json({ message: 'No thought with this id!' });
       }
 
-      await Thought.deleteMany({ _id: { $in: user.thoughts } });
-      res.json({ message: 'Thoughts and users deleted!' });
+      const user = await User.findOneAndUpdate(
+        { thoughts: req.params.id },
+        { $pull: { thoughts: req.params.id } },
+        { new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({
+          message: 'Thought deleted but no user with this id!',
+        });
+      }
+
+      res.json({ message: 'Thought successfully deleted!' });
     } catch (err) {
       res.status(500).json(err);
     }
   },
   // Update a user
-  async updateUser(req, res) {
+  async updateThought(req, res) {
     try {
-      const user = await User.findOneAndUpdate(
+      const thought = await Thought.findOneAndUpdate(
         { _id: req.params.id },
         { $set: req.body },
         { runValidators: true, new: true }
       );
 
-      if (!user) {
-        res.status(404).json({ message: 'No user with this id!' });
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
       }
 
-      res.json(user);
+      res.json(thought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
+    }
+  },
+  
+  // Adds a reaction to a thought. This method is unique in that we add the entire body of the tag 
+  // rather than the ID with the mongodb $addToSet operator.
+  async addReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $addToSet: { reactions: req.body } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+
+      res.json(thought);
     } catch (err) {
       res.status(500).json(err);
     }
   },
+
+  // Remove thought reaction. This method finds the application based on ID. 
+  // It then updates the tags array associated with the thought in question by removing it's reactionId 
+  // from the reaction array.
+  async removeReaction(req, res) {
+    try {
+      const thought = await Thought.findOneAndUpdate(
+        { _id: req.params.thoughtId },
+        { $pull: { reactions: { reactionId: req.params.reactionId } } },
+        { runValidators: true, new: true }
+      );
+
+      if (!thought) {
+        return res.status(404).json({ message: 'No thought with this id!' });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -5,16 +5,24 @@ const {
   getThoughts,
   getThoughtById,
   updateThought,
-  deleteThought
+  deleteThought,
+  addReaction,
+  removeReaction,
 } = require('../../controllers/thoughtController.js');
 
 // /api/thoughts
 router.route('/').get(getThoughts).post(createThought);
 
-// /api/users/:id
+// /api/thoughts/:id
 router.route('/:id')
 .get(getThoughtById)
 .put(updateThought)
 .delete(deleteThought)
+
+// /api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
 
 module.exports = router;
